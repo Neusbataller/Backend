05a18c8a9db1d9ff13968e7ea2779338b62fe4dd
Neusbataller <nebaib@alumnatflorida.es>
2025-03-14 16:27:23 +0100

    index

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+//Settings
 app.set("port", process.env.PORT || 8080);
 
-app.listen(app.get("port"), () => {
-  console.log("hola desde el puerto: " + app.get("port"));
-});
+//Middleware
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
 //Rutas
 app.use(require("./routes/index"));
+
+//Iniciando el servidor
+app.listen(app.get("port"), () => {
+  console.log("hola desde el puerto: " + app.get("port"));
+});
